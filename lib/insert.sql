6e265a91442e960f58cc0c9f525546cb54ea5211
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-12-22 20:11:19 -0500

    commit tree b2052736f8b783e05cd1c96a1a9f8f462e56e210

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'BROWN', 'LOVELY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'Black', 'LOVELY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Silver', 'LOVELY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 18, 'F', 'GREEN', 'STRICT', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'BROWN', 'SLEEPY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 666, 'F', 'Chartruese', 'chaotic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 85, 'M', 'IRIDESCENT', 'Ambivalent', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7685, 'M', 'BROWN', '???', 1);
+
+
+
+
